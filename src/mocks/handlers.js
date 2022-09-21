02427e442c52aeed70e386591345560aabe69592
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-09-21 20:10:05 -0300

    feat: updated paths

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,16 +1,17 @@
 import { rest } from 'msw';
 export const handlers = [
-    rest.post('/api/login', (req, res, ctx) => {
+    rest.post('api/login', (req, res, ctx) => {
       // Persist user's authentication in the session
+
       //sessionStorage.setItem('is-authenticated', 'true')
       return res(
         // Respond with a 200 status code
         ctx.status(200),
       )
     }),
-    rest.get('/user', (req, res, ctx) => {
+    rest.get('user', (req, res, ctx) => {
       // Check if the user is authenticated in this session
-      const isAuthenticated = sessionStorage.getItem('is-authenticated')
+      const isAuthenticated =  true //sessionStorage.getItem('is-authenticated')
       if (!isAuthenticated) {
         // If not authenticated, respond with a 403 error
         return res(
