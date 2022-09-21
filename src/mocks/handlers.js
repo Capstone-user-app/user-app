34166463a2a60421eb37bbc218247aa379204b3a
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-09-21 19:40:56 -0300

    feat: updated path

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,8 @@
 import { rest } from 'msw';
 export const handlers = [
-    rest.post('/login', (req, res, ctx) => {
+    rest.post('/api/login', (req, res, ctx) => {
       // Persist user's authentication in the session
-      sessionStorage.setItem('is-authenticated', 'true')
+      //sessionStorage.setItem('is-authenticated', 'true')
       return res(
         // Respond with a 200 status code
         ctx.status(200),
