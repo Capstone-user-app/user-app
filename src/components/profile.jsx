a79d90905420a931adf08b826a77eca5c2151ab7
vicentesh <48304636+vicentesh@users.noreply.github.com>
2022-09-26 19:55:27 -0300

    style(profile.jsx)

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,14 +1,15 @@
-import { useAuth0 } from "@auth0/auth0-react"
-import React from "react"
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
 
 
 const Profile = () => {
     const { user, isAuthenticated, isLoading } = useAuth0()
-  
+
     if (isLoading) {
-      return <div>Loading ...</div>;
+      // eslint-disable-next-line react/jsx-filename-extension
+      return <div>Loading ...</div>
     }
-  
+
     return (
       isAuthenticated && (
         <div>
@@ -17,7 +18,7 @@ const Profile = () => {
           <p>{user.email}</p>
         </div>
       )
-    );
-  };
-  
+    )
+  }
+
   export default Profile
