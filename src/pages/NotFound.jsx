43b8cee274380507d5d38148bf87bc1b9fab5d40
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-10-05 10:04:11 -0300

    feat: done css, page is responsive

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-        <section className="dark:bg-gray-900 dark:text-gray-100 flex h-full items-center p-16">
-            <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
-                <div className="max-w-md text-center">
-                    <h2 className="dark:text-gray-600 mb-8 text-9xl font-extrabold">
+        <section className="flex h-full items-center p-8">
+            <div className="container mx-auto flex w-5/6 flex-col items-center justify-center">
+                <div className="w-full text-center md:w-1/2">
+                    <h2 className="mb-8 text-7xl font-extrabold md:text-9xl">
                         <span className="sr-only">Error</span>404
                     </h2>
-                    <p className="text-xl font-semibold md:text-3xl">Lo lamentamos, no pudimos encontrar la página que estás buscando.</p>
-                    <p className="dark:text-gray-400 mt-4 mb-8">Pero no te preocupes, todavía puedes encontrar un monton de cosas de nuestra página principal.</p>
-                    <a href="/" rel="noopener noreferrer" className="dark:bg-violet-400 dark:text-gray-900 rounded px-8 py-3 font-semibold">Devuleta a la pagina principal</a>
+                    <p className=" mt-4 mb-8 font-semibold md:text-2xl">Lo lamentamos, no pudimos encontrar la página que estabas buscando.</p>
+                    <a href="/" rel="noopener noreferrer" className="rounded bg-lblue py-2 px-4 text-sm font-semibold text-white hover:shadow-xl md:text-lg">Devuelta a la pagina principal</a>
                 </div>
             </div>
         </section>
